028ef7fb486cef9c5a644b2570bc49394b05e5b4
Lenovo <li_louni@esi.dz>
2023-12-24 09:31:38 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        'light': '0 2px 4px rgba(0, 0, 0, 0.03)',
+      },
+      colors: {
+        black: '#1E1E1E',
+        yellow: '#FFBE00',
+        blue: '#50B3C5',
+        red: '#C93227',
+        grey: '#EEEEEE',
+      },
+    },
+  },
+  plugins: [],
+}
+
