10ff28456dd26be8df77486a8b23dc630b1b2a30
Zola456 <zolaguedda16@gmail.com>
2023-12-25 13:07:57 +0100

    pages client

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-<<<<<<< HEAD
+
 import './App.css'
 import GererModerateursPage from './Pages/Admin/GererModerateursPage'
 import ListeArticles from './Pages/Moderateur/ListeArticles'
 import DeleteModerateurPopUp from './Components/DeleteModerateurPopUp'
 import Welcome from './Pages/Client/Welcome'
 import Recherche from './Pages/Client/Recherche'
-=======
-import "./App.css";
-import ListeArticles from "./Pages/Moderateur/ListeArticles";
->>>>>>> 5b41803fe56d6bbc68423ebdd81bf995a236ca4d
+
 
 function App() {
   return (
     <div className="">
-      <Welcome />
+      <Recherche />
     </div>
   );
 }
