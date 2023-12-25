a99dae96ecef3d86a990c7b00229b21d3b5d6df3
Zola456 <zolaguedda16@gmail.com>
2023-12-25 11:07:54 +0100

    pages client

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css'
 import GererModerateursPage from './Pages/Admin/GererModerateursPage'
 import ListeArticles from './Pages/Moderateur/ListeArticles'
 import DeleteModerateurPopUp from './Components/DeleteModerateurPopUp'
+import Welcome from './Pages/Client/Welcome'
+import Recherche from './Pages/Client/Recherche'
 
 function App() {
   return (
     <div className="">
-      <ListeArticles />
+      <Welcome />
     </div>
   );
 }
---
+++ src/Pages/Client/Welcome.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './welcome.css'; 
+
+
+function Welcome() {
+
+
+    return (
+
+    
+<div className="container">
+<div className="title">
+  <span className="black">WELCOME TO </span>
+  <span className="yellow">S</span>
+  <span className="black">CI</span>
+  <span className="blue">EN</span>
+  <span className="red">S</span>
+  <span className="black">PACE</span>
+</div>
+<div className="main-text">
+  <div className="center-text">
+   <p>
+    Welcome to science space, a collaborative project that empowers users to delve into the world of scientific literature with ease.
+    Welcome to science space, collaborative project that empowers users to delve into the world of scientific literature with ease.
+   </p>
+  </div> 
+  <div className="buttons">
+    <button className="blue-button">Login</button>
+    <button className="blue-button">Create Account</button>
+  </div>
+</div>
+</div>
+
+
+
+
+
+
+
+    
+    );
+  }
+
+  
+  export default Welcome;
+  
