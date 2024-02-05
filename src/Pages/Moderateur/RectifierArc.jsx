2f03cab3a23ce0ce95ffedcbe42d459429827981
Lenovo <li_louni@esi.dz>
2024-02-05 18:27:34 +0100

    articles styling fixed

--- src/Pages/Moderateur/RectifierArc.jsx
+++ src/Pages/Moderateur/RectifierArc.jsx
@@ -1,176 +1,11 @@
-// import React, { useState } from "react";
-// import './MdetailsAr.css' ;
-// import { useEffect } from 'react';
-// import { useParams } from 'react-router-dom';
-
-// function RectifierArc() {
-
-//   const [isFavorite, setIsFavorite] = useState(false);
-//   const [isDeleteClicked, setIsDeleteClicked] = useState(false);
-//   const [isEditClicked, setIsEditClicked] = useState(false);
-
-//   const toggleFavorite = () => {
-//     setIsFavorite(!isFavorite);
-//   };
-
-//   const handleDeleteClick = () => {
-//     setIsDeleteClicked(!isDeleteClicked);
-//   };
-
-//   const handleEditClick = () => {
-//     setIsEditClicked(!isEditClicked);
-//   };
-
-//   const { id } = useParams();
-//   const [rectifierArc, setRectifierArc] = useState([]);
-//   const [references, setReferences] = useState([]);
-//   const [auteurs, setAuteurs] = useState([]);
-
-//   useEffect(() => {
-//     const url = `http://127.0.0.1:8000/api/articles/${id}/`;
-//     console.log('testing');
-//     fetch(url)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         console.log("Data received:",data.Article);
-//         console.log("refernces:",data.Article.references);
-
-//         setRectifierArc(data.Article);
-//         setReferences(data.Article.references);
-//         setAuteurs(data.Article.auteurs);
-
-//       });
-//   }, [id]); // Empty dependency array to run the effect only once on mount
-
-//   return (
-//     <div className="min-h-screen relative ">
-//       <h1 className="text-4xl font-bold text-black absolute md:top-40 text-center md:right-30 mt-20">
-//        Rectifier Article
-//       </h1>
-//       <div className="  relative">
-//         <a href={rectifierArc && rectifierArc.urlPdf} className="text-blue-500 absolute md:top-60 md:right-30 ml-80 ">
-//           [VIEW PDF]
-//         </a>
-//       </div>
-//       <span onClick={toggleFavorite} className="cursor-pointer ml-2 absolute top-0 right-0 mt-8 mr-8">
-//         {isFavorite ? (
-//           <svg
-//             xmlns="http://www.w3.org/2000/svg"
-//             className="h-6 w-6 text-yellow-500"
-//             fill="none"
-//             viewBox="0 0 24 24"
-//             stroke="currentColor"
-//           >
-//             <path
-//               strokeLinecap="round"
-//               strokeLinejoin="round"
-//               strokeWidth={2}
-//               d="M12 14l9-5-9-5-9 5 9 5z"
-//             />
-//           </svg>
-//         ) : (
-//           <svg
-//             xmlns="http://www.w3.org/2000/svg"
-//             className="h-6 w-6 text-white"
-//             fill="none"
-//             viewBox="0 0 24 24"
-//             stroke="currentColor"
-//           >
-//             <path
-//               strokeLinecap="round"
-//               strokeLinejoin="round"
-//               strokeWidth={2}
-//               d="M12 14l9-5-9-5-9 5 9 5z"
-//             />
-//           </svg>
-//         )}
-//       </span>
-//       <div className=" min-h-screen flex items-center ml-80 ">
-//         <div className=" font-semibold text-black text-left  ">
-
-//           <div className="input-container">
-//             <strong>Titre Article:</strong>
-//             <input className="input-large" type="text" value={rectifierArc && rectifierArc.titre}/>
-//           </div>
-
-//           <div className="input-container">
-//   <strong>Auteurs:</strong>
-//   {auteurs
-//     ? auteurs.map((auteur, index) => (
-//         <div key={index}>
-//           <input
-//             className="input-large"
-//             type="text"
-//             value={`${"\u2022"} ${auteur.full_name}, ${auteur.email}, ${auteur.institution.nom}, ${auteur.institution.adress}`}
-
-//           />
-//         </div>
-//       ))
-//     : null}
-// </div>
-
-//          <div className="input-container">
-//             <strong>Mots cle:</strong>
-//             <input className="input-large" type="text" value={rectifierArc && rectifierArc.motsCles} />
-//           </div>
-
-//           <div className="input-container">
-//             <strong>Resume:</strong>
-//             <input className="input-large" type="text" value={rectifierArc && rectifierArc.resume} />
-//           </div>
-
-//           <div className="input-container">
-//             <strong>Texte Integral:</strong>
-//             <input className="input-large" type="text" value={rectifierArc && rectifierArc.texteIntegral} />
-//           </div>
-
-//           <div className="input-container">
-//             <strong>Réference:</strong>
-
-//              {references
-//     ? references.map((reference, index) => (
-//         <div key={index}>
-//           <input
-//             className="input-large"
-//             type="text"
-//             value={`${"\u2022"} ${reference.titre}`}
-
-//           />
-//         </div>
-//       ))
-//     : null}
-//           </div>
-
-//           <div className="buttons">
-//             <button
-//                className={`red-button ${isDeleteClicked ? "gray-bg" : ""}`}
-//               onClick={handleDeleteClick}
-//             >
-//               Annuler changements
-//             </button>
-//             <button
-//               className={`blue-button ${isEditClicked ? "gray-bg" : ""}`}
-//               onClick={handleEditClick}
-//             >
-//               Valider changements
-//             </button>
-//           </div>
-
-//           {/* Ajoutez le reste du texte avec le formatage approprié */}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default RectifierArc;
-
 import React, { useState, useEffect } from "react";
 import "./MdetailsAr.css";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { routers } from "../../endpoints";
+import bgtp1 from "../../assets/bgtp1.jpg";
+
 
 function RectifierArc() {
   const navigate = useNavigate();
@@ -257,9 +92,13 @@ function RectifierArc() {
       });
   };
 
+  const backgroundImageStyle = {
+    backgroundImage: `url(${bgtp1})`,
+  };
+
   return (
-    <div className="min-h-screen relative">
-      <h1 className="text-4xl font-bold text-black absolute md:top-40 text-center md:right-30 mt-20">
+    <div style={backgroundImageStyle} className='min-h-screen relative text-[#1E1E1E] font-["Inter"]'>
+      <h1 className="text-4xl font-bold ml-80 mt-24">
         Rectifier Article
       </h1>
       <form onSubmit={handleSubmit}>
@@ -268,7 +107,7 @@ function RectifierArc() {
             <div className="input-container">
               <strong>Titre Article:</strong>
               <input
-                className="input-large"
+                className="w-full border-2 border-green rounded my-4 p-4 w-full text-green bg-dark-blue"
                 type="text"
                 name="titre"
                 value={formData.titre}
@@ -278,9 +117,9 @@ function RectifierArc() {
             </div>
 
             <div className="input-container">
-              <strong>Mots cle:</strong>
+              <strong>Mots clés:</strong>
               <input
-                className="input-large"
+                className="w-full border-2 border-green rounded my-4 p-4 w-full text-green bg-dark-blue"
                 type="text"
                 name="motsCles"
                 value={formData.motsCles}
@@ -290,9 +129,9 @@ function RectifierArc() {
             </div>
 
             <div className="input-container">
-              <strong>Resume:</strong>
+              <strong>Resumé:</strong>
               <input
-                className="input-large"
+                className="w-full border-2 border-green rounded my-4 p-4 w-full text-green bg-dark-blue"
                 type="text"
                 name="resume"
                 value={formData.resume}
@@ -303,8 +142,8 @@ function RectifierArc() {
 
             <div className="input-container">
               <strong>Texte Integral:</strong>
-              <input
-                className="input-large"
+              <textarea
+                className="w-full border-2 border-green rounded my-4 p-4 w-full text-green bg-dark-blue"
                 type="text"
                 name="texteIntegral"
                 value={formData.texteIntegral}
@@ -319,7 +158,7 @@ function RectifierArc() {
                 <>
                   <div key={index}>
                     <input
-                      className="input-large"
+                      className="w-full border-2 border-green rounded my-4 p-4 w-full text-green bg-dark-blue"
                       type="text"
                       name="full_name"
                       value={auteur.full_name}
@@ -327,7 +166,8 @@ function RectifierArc() {
                       style={{ border: "1px solid gray", padding: "5px" }}
                     />
                     <input
-                      className="input-large"
+                      className="w-full border-2 border-green rounded my-4 p-4 w-full text-green bg-dark-blue"
+
                       type="text"
                       name="email"
                       value={auteur.email}
@@ -335,7 +175,8 @@ function RectifierArc() {
                       style={{ border: "1px solid gray", padding: "5px" }}
                     />
                     <input
-                      className="input-large"
+                      className="w-full border-2 border-green rounded my-4 p-4 w-full text-green bg-dark-blue"
+
                       type="text"
                       name="institution_nom"
                       value={auteur.institution.nom}
@@ -343,7 +184,8 @@ function RectifierArc() {
                       style={{ border: "1px solid gray", padding: "5px" }}
                     />
                     <input
-                      className="input-large"
+                      className="w-full border-2 border-green rounded my-4 p-4 w-full text-green bg-dark-blue"
+
                       type="text"
                       name="institution_adress"
                       value={auteur.institution.adress}
@@ -361,7 +203,7 @@ function RectifierArc() {
               {formData.references.map((reference, index) => (
                 <div key={index}>
                   <input
-                    className="input-large"
+                    className="w-full border-2 border-green rounded p-4 w-full text-green bg-dark-blue"
                     type="text"
                     name="titre"
                     value={reference.titre}
@@ -372,13 +214,13 @@ function RectifierArc() {
               ))}
             </div>
 
-            <div className="buttons">
+            <div className="buttons flex gap-5">
               <Link to={`${routers.MDETAILS}/${id}`}>
-                <button className={`red-button`}>Annuler changements</button>
+                <button className={`red-button flex items-center justify-center w-full h-8 p-8 rounded-[0.15em]`}>Annuler changements</button>
               </Link>
 
               <button
-                className={`blue-button`}
+                className={`flex items-center justify-center bg-[#50B3C5] h-8 p-8 rounded-[0.15em] text-white`}
                 type="submit"
                 onClick={handleSubmit}
               >
